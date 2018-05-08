f269ec9fa9f354994b59b5968823d4832419e383
SashaYerashoff <a.yerashoff@gmail.com>
2018-05-08 20:37:27 +0300

    Funkcija_ar_parametru
    Convert.To bez parbaudes

--- day_3/day_3/Program.cs
+++ day_3/day_3/Program.cs
@@ -6,10 +6,23 @@ namespace day_3
     {
         static void Main(string[] args)
         {
+
+
 			int mainigais = Funkcija2(); //return f-cijas rezultātu ieveto mainīgajā
 			Console.WriteLine(mainigais); //return f-cijas mainīgā izvade
 
+			int a, b;
+			Console.Write("ievadi a:");
+			a = Convert.ToInt32(Console.ReadLine());
+
+			Console.Write("ievadi b:");
+            b = Convert.ToInt32(Console.ReadLine());
+
+			int fcija4 = Funkcija4(a, b);
+			Console.WriteLine("F-cija4 rezultats:" + fcija4);
+
 			Funkcija(); //void f-cijas palaišana
+			Funkcija3(5, 6);
 
 			Console.ReadLine(); //"pauze" windows consolei
 
@@ -18,7 +31,7 @@ namespace day_3
 		{
 			int a, b, c;
 			a = 3;
-			b = 3;
+			b = 2;
 			c = a + b;
 
 			Console.WriteLine(c);
@@ -30,6 +43,19 @@ namespace day_3
             a = 2;
             b = 3;
             c = a + b;
+
+			return c;
+		}
+
+		static void Funkcija3(int skaitlis1, int skaitlis2)
+		{
+			int c = skaitlis1 + skaitlis2;
+			Console.WriteLine("f-cija ar parametru " + c);
+
+		}
+		static int Funkcija4(int skaitlis1, int skaitlis2)
+		{
+			int c = skaitlis1 + skaitlis2;
 
 			return c;
 		}
