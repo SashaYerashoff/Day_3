5b3283c63e9945821aa6d09a2fd6e03aff4e581d
SashaYerashoff <a.yerashoff@gmail.com>
2018-05-08 19:29:52 +0300

    voidVSreturn

--- day_3/day_3/Program.cs
+++ day_3/day_3/Program.cs
@@ -6,7 +6,32 @@ namespace day_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+			int mainigais = Funkcija2(); //return f-cijas rezultātu ieveto mainīgajā
+			Console.WriteLine(mainigais); //return f-cijas mainīgā izvade
+
+			Funkcija(); //void f-cijas palaišana
+
+			Console.ReadLine(); //"pauze" windows consolei
+
         }
+		static void Funkcija()
+		{
+			int a, b, c;
+			a = 2;
+			b = 3;
+			c = a + b;
+
+			Console.WriteLine(c);
+		}
+
+		static int Funkcija2()
+		{
+			int a, b, c;
+            a = 2;
+            b = 3;
+            c = a + b;
+
+			return c;
+		}
     }
 }
